a7b789ec74c420576bc420455e8bb2d6f9dc2924
Rijas Abdul Razak <107810873+RijasAbdulRazak@users.noreply.github.com>
2026-01-23 16:41:13 +0530

    button push

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -10,7 +10,7 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({ text, onClick, className 
   return (
     <button
       onClick={onClick}
-      className={`px-[24px] py-[16px] bg-red clamp-[mt,12px,32px] clamp-[text,button-m,button-d] clamp-[leading,button-m,button-d] font-semibold ${className}`}
+      className={`px-[24px] py-[16px] text-white bg-red clamp-[mt,12px,32px] clamp-[text,button-m,button-d] clamp-[leading,button-m,button-d] font-semibold ${className}`}
     >
       {text}
     </button>
